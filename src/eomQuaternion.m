f1122e75b7389a9d8a888379f576413e2ee3e456
Obedience Adara <166041189+ObedienceAdara@users.noreply.github.com>
2026-09-09 08:06:58 +0100

    Add quaternion equations of motion

---
+++ src/eomQuaternion.m
@@ -0,0 +1,36 @@
+function xdot = eomQuaternion(t, x, aircraft, controlHistory, uNominal, isRunning)
+%EOMQUATERNION Quaternion 6-DOF equations of motion.
+arguments
+    t (1,1) double
+    x (13,1) double
+    aircraft (1,1) Aircraft
+    controlHistory (1,1) ControlHistory
+    uNominal (7,1) double
+    isRunning (1,1) logical
+end
+HEB = RMQ(x(10), x(11), x(12), x(13));
+x(6)=min(x(6),0);
+[airDens,~,~,~]=Atmosphere(-x(6));
+Phi=atan2(2*(x(10)*x(13)+x(11)*x(12)),1-2*(x(10)^2+x(11)^2));
+Theta=asin(2*(x(11)*x(13)-x(10)*x(12)));
+Psi=atan2(2*(x(12)*x(13)+x(10)*x(11)),1-2*(x(11)^2+x(12)^2));
+windb=WindField(x(3),Phi,Theta,Psi);
+gb=HEB*[0;0;9.80665];
+x(1)=max(x(1),0);
+Va=[x(1);x(2);x(3)]+windb;
+V=sqrt(Va'*Va); alphar=atan(Va(3)/abs(Va(1))); betar=asin(Va(2)/V);
+uTotal=controlHistory.totalControl(t,uNominal,isRunning);
+[CD,CL,CY,Cl,Cm,Cn,Thrust]=aircraft.aeroCoefficients(x,uTotal,alphar,betar,V);
+qbarS=0.5*airDens*V^2*aircraft.WingArea;
+CX=-CD*cos(alphar)+CL*sin(alphar); CZ=-CD*sin(alphar)-CL*cos(alphar);
+Xb=(CX*qbarS+Thrust)/aircraft.Mass; Yb=CY*qbarS/aircraft.Mass; Zb=CZ*qbarS/aircraft.Mass;
+Lb=Cl*qbarS*aircraft.WingSpan; Mb=Cm*qbarS*aircraft.MeanChord; Nb=Cn*qbarS*aircraft.WingSpan;
+xd1=Xb+gb(1)+x(9)*x(2)-x(8)*x(3); xd2=Yb+gb(2)-x(9)*x(1)+x(7)*x(3); xd3=Zb+gb(3)+x(8)*x(1)-x(7)*x(2);
+y=HEB'*[x(1);x(2);x(3)]; xd4=y(1); xd5=y(2); xd6=y(3);
+Ixx=aircraft.Ixx; Iyy=aircraft.Iyy; Izz=aircraft.Izz; Ixz=aircraft.Ixz;
+xd7=(Izz*Lb+Ixz*Nb-(Ixz*(Iyy-Ixx-Izz)*x(7)+(Ixz^2+Izz*(Izz-Iyy))*x(9))*x(8))/(Ixx*Izz-Ixz^2);
+xd8=(Mb-(Ixx-Izz)*x(7)*x(9)-Ixz*(x(7)^2-x(9)^2))/Iyy;
+xd9=(Ixz*Lb+Ixx*Nb+(Ixz*(Iyy-Ixx-Izz)*x(9)+(Ixz^2+Ixx*(Ixx-Iyy))*x(7))*x(8))/(Ixx*Izz-Ixz^2);
+p=x(7); q=x(8); r=x(9); Q=0.5*[0 r -q p;-r 0 p q;q -p 0 r;-p -q -r 0]; qd=Q*[x(10);x(11);x(12);x(13)];
+xdot=[xd1;xd2;xd3;xd4;xd5;xd6;xd7;xd8;xd9;qd(1);qd(2);qd(3);qd(4)];
+end
